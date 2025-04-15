a693d8cdf7da7af0fb7f1385f72a6353ee1cf074
RishabhSharma023 <rishsharma023@gmail.com>
2025-04-15 14:05:12 -0400

    Pushing to git b4 wipe

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from './Components/Hero.jsx';
 import Featured from './Components/Feature.jsx';
 import Footer from './Components/Footer.jsx';
 
-function App() {
+/*function App() {
     return (
         <div className="app">
             <Header />
@@ -13,6 +13,42 @@ function App() {
             <Footer />
         </div>
     );
+}
+
+export default App;*/
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+import './App.css';
+
+function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${import.meta.env.VITE_TMDB_KEY}`);
+      setMovies(response.data.results);
+    }
+  
+    getData();
+  }, []);
+
+  return (
+    <div className="movie-container">
+            <Header />
+            <Hero />
+            <Featured />
+            <Footer />
+      {movies && movies.map(movie => (
+        <div className="movie-card" key={movie.id}>
+          <h1>{`${movie.title}`}</h1>
+          <img className="movie-poster" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`${movie.id}`} />
+        </div>
+      ))}
+    </div>
+
+    
+  );
 }
 
 export default App;
