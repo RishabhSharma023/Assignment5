92486e34e155adb738df48e88e4f9897343921a7
RishabhSharma023 <rishsharma023@gmail.com>
2025-04-24 13:50:32 -0400

    commit before pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,28 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './Components/Header.jsx';
-import Hero from './Components/Hero.jsx';
-import Featured from './Components/Feature.jsx';
-import Footer from './Components/Footer.jsx';
 import axios from "axios";
-import HomeView from ".Views/HomeView.jsx"
-import RegisterView from ".Views/RegisterView.jsx"
-import LoginView from ".Views/LoginView.jsx"
-import MoviesView from ".Views/MoviesView.jsx"
-import DetailedView from ".Views/DetailedView.jsx"
-import GenreView from ".Views/GenreView.jsx"
+import { useEffect, useState } from "react";
+import './App.css';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      setMovies((await axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${import.meta.env.VITE_TMDB_KEY}`)).data.results);
+    };
+
+    getData();
+  }, []);
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomeView />} />
-        <Route path="/register" element={<RegisterView />} />
-        <Route path="/login" element={<LoginView />} />
-        <Route path="/movies" element={<MoviesView />}>
-          <Route path="genre/:genre_id" element={<GenreView />} />
-          <Route path="details/:id" element={<DetailedView />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  )
+    <div className="movie-container">
+      {movies && movies.map(movie => (
+        <div className="movie-card" key={movie.id}>
+          <h1>{`${movie.title}`}</h1>
+          <img className="movie-poster" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`${movie.id}`} />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
